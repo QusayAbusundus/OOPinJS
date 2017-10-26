6944a6b3fe73a74658b35b138a537f8689e676a6
QusayAbusundus <32073337+QusayAbusundus@users.noreply.github.com>
2017-10-26 13:39:55 -0400

    made a bubbles array

--- sketch.js
+++ sketch.js
@@ -1,14 +1,23 @@
-let bubble
+let bubbles = [];
 
 function setup() { // built-in P5.JS function -=- this runs once
 	createCanvas(600, 400);
-	bubble = new Bubble(300, 200, 32);
+	for(let i = 0; i < 50; i++)
+	{
+		let x = random(width);
+		let y = random(10, 150);
+		let radius = random(10, 20);
+		bubbles[i] = new Bubble(x, y, radius);
+	}
 }
 
 function draw() { // built-in P5.JS function -=-  automatic loop that repeats forever
 	background(0); // give the canvas a black background
-	bubble.move();
-	bubble.show();
+	for(let i = 0; i < bubbles.length; i++)
+	{
+		bubbles[i].move();
+		bubbles[i].show();
+	}
 }
 
 class Bubble
@@ -23,13 +32,13 @@ class Bubble
 	move()
 	{
 		this.x = this.x + random(-5, 5);
-		this.y = this.y + random(-5, 5);
+		this.y = this.y + random(1, 5);
 	}
 
 	show()
 	{
 		stroke(255);
-		strokeWeight(4);
+		strokeWeight(2);
 		noFill();
 		ellipse(this.x, this.y, (this.r*2), (this.r*2));
 	}
